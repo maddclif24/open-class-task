5560c9405bc96e19d350d6262627377763f1473a
maddclif <maddclif24@gmail.com>
2021-06-17 12:50:19 +0300

    add reg form

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 import Express from 'express';
 import morgan from 'morgan';
-import fs from 'fs';
-
 
 const app = new Express();
-const accessLogStream = fs.createWriteStream('./access.log', { flags: 'a' });
-const logger = morgan('combined', { stream: accessLogStream });
-
+const logger = morgan('combined');
 
 app.use(logger);
-app.use(Express.json());
-app.use(Express.static('public'));
+//app.use(Express.static('public'));
 app.use(Express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('./public/index.html');
+    res.render('index.html');
 });
 
 app.listen(8080, () => {
